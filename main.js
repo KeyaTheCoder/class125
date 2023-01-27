7692ecbe4b73be3b3766cf77b17de980167f93ef
KeyaTheCoder <102337733+KeyaTheCoder@users.noreply.github.com>
2023-01-26 18:43:54 -0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,40 @@
+noseX=0;
+noseY=0;
+function preload() {
+Lipstick= loadImage('https://i.postimg.cc/PxFvYgkv/l1.png');
+}
+
+function setup() {
+    canvas = createCanvas(300, 300);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(300, 300);
+    video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function gotPoses(results) {
+    if(results.length > 0)
+    {
+        console.log(results);
+        noseX = results[0].pose.nose.x;
+        noseY = results[0].pose.nose.y;
+        console.log("nose x = " + results[0].pose.nose.x);
+        console.log("nose y = " + results[0].pose.nose.y);
+    }
+}
+
+function modelLoaded() {
+    console.log('PoseNet Is Initialized');
+}
+
+function draw() {
+    image(video, 0, 0, 300, 300);
+    image(Lipstick, noseX-15, noseY+5, 30, 20);
+}
+
+function take_snapshot() {
+    save('myfliterimage.png');
+}
